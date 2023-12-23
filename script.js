2dbd5d9c4fa9793ad272d3a52b0e05abeda739a2
nicopressez <154272808+nicopressez@users.noreply.github.com>
2023-12-23 20:17:20 +0100

    Added grid's color change when mouse hover
    When hovering over any of the grid cells, the background color of this cell changes

--- script.js
+++ script.js
@@ -8,8 +8,19 @@ function makeGrid(rows, columns)
 
     for (let index = 0; index < (rows * columns); index++) {
         const gridCell = document.createElement('div');
-        container.appendChild(gridCell).className="grid-item"
+        container.appendChild(gridCell).className="grid-item";
     }
 }
 
 makeGrid(16,16);
+
+const gridCells = document.getElementsByClassName("grid-item");
+function changeBackground(color) {
+    this.style.setProperty("background-color", color); 
+}
+
+for (let i = 0; i < gridCells.length; i++) {
+    gridCells[i].addEventListener('mouseover', function() {
+        changeBackground.call(this, 'black'); 
+    });
+}
